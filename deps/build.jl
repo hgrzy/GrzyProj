2526c3b0d494641b24f851ebda527014f418b471
hgrzy <hannahg0104@gmail.com>
2024-01-11 21:38:02 -0500

    Temporary chg for deps for debugging purposes

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,5 @@
-include("../src/get_planet_data.jl")
+# include("../src/get_planet_data.jl")
 println("Downloading NASA JPL Horizons Data for Sun and Planets...")
-bodies = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-download_planet_data_from_list(bodies)
-println("NASA JPL Horizons Data for Sun and Planets downloaded.")
+# bodies = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+# download_planet_data_from_list(bodies)
+# println("NASA JPL Horizons Data for Sun and Planets downloaded.")
